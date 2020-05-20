97e868b8d578162e5f2dbc2ec47620a2d5230664
Caio Marcilio dos Santos <60391947+Arkdroid04@users.noreply.github.com>
2020-05-20 10:30:25 -0300

    Receive data with better format
    Chaged how the code receive data from ComponentDB database.

--- get_all_item_Test.py
+++ get_all_item_Test.py
@@ -1,5 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 ‎09:30:22 2020
+
+Receptor de dados do ComponentDB
+
+@author: caiom
+"""
+
 from cdb.cdb_web_service.api.itemRestApi import ItemRestApi
 from cdb.common.exceptions.invalidRequest import InvalidRequest
+import pandas as pd
+from io import StringIO
+import numpy as np
 
 #Reads a file with the credentials and save in the "line" variables
 data = open('account_cdb.txt', 'r')
@@ -11,7 +23,15 @@ port = 10232
 protocol = line[4]
 data.close
 
+#Log into CDB database and get all catalog items with all properties
 login = ItemRestApi(user, password, server, port, protocol)
-item = login.getCatalogItems()
+catalog = login.getCatalogItems()
 
-print(item)
+
+x = 0
+for item in catalog:
+    y = 'item'
+    exec(y+str(x)+'=catalog[x]')
+    exec('del '+ y+str(x) +'[' + 'u' + "'domain'" +']')
+    exec('print('+ y+str(x) +'.values())')
+    x += 1
