bbe867b8230ad49abee5fef5a103ca1d32952cfc
Arkdroid04 <60391947+Arkdroid04@users.noreply.github.com>
2020-08-07 09:24:10 -0300

    Reworked in the data receiver loop

--- get_catalog_item.py
+++ get_catalog_item.py
@@ -11,9 +11,8 @@ from cdb.cdb_web_service.api.itemRestApi import ItemRestApi
 from cdb.common.exceptions.invalidRequest import InvalidRequest
 import pandas as pd
 import getpass
-from collections import OrderedDict
 
-print("\n"+"**"*20+" CDB CATALOG ITEMS "+"**"*20+"\n\nINSERT CDB CREDENTIALS")
+print("INSERT CDB CREDENTIALS\n")
 
 #Reads the credentials and save in the variables to connect to the server
 protocol = raw_input("\nNetwork protocol: ")
@@ -23,39 +22,31 @@ user = raw_input("CDB user name: ")
 password = getpass.getpass(prompt = "Password: ")
 print("\nLoading data...\n")
 
-#Log into CDB database and get all catalog items with all properties
+#Log into CDB database
 login = ItemRestApi(user, password, server, port, protocol)
 
-#Defines a function to get catalog from the server and store in a list of lists
+#Defines a function to get catalog from the server
 def get_catalog():
-    global catalog
-    global cat_list
-    global cat_table
-    global cat_id
 
 #Calls the function from CDB API
+    print('Loading catalog items...\n')
     catalog = login.getCatalogItems()
-    w = 'item'
-    x = 0
-    y = 'cat'
-    z = 0
     cat_list = []
 
-#loop to map the catalog item data dictionaries and convert into lists which will be saved in the memory
-    for item in catalog:
-        exec(w+str(x)+'=catalog[x]')
-        exec("if u'domain' in "+w+str(x)+": del "+w+str(x)+"[u'domain']")
-        exec("if u'item_identifier2' in "+ w+str(x)+ ": del "+ w+str(x) +"[u'item_identifier2']")
-        exec(y+str(z)+'=OrderedDict(sorted('+w+str(x)+'.items(), reverse=True))')
-        exec("cat_list.append("+y+str(z)+".values())")
-        x += 1
-        z += 1
+#loop to map the catalog item data dictionaries and convert into lists
+    for i in range(0, len(catalog)):
+        if u'domain' in catalog[i]:
+            del catalog[i][u'domain']
+        if u'item_identifier2' in catalog[i]:
+            del catalog[i][u'item_identifier2']
 
-#Defines a function to receive the catalog and print it
-def print_catalog():
-    get_catalog()
-    cat_table = pd.DataFrame(cat_list, columns = ['Item Name', 'Item Identifier', 'Item ID', 'Item Domain'])
-    print("**"*20+" CATALOG ITEMS LIST "+"**"*20+"\n")
-    print(cat_table)
+        item = [catalog[i][u'name'], catalog[i][u'item_identifier1'], catalog[i][u'id'], catalog[i][u'domain_id']]
+        cat_list.append(item)
 
-print_catalog()
+    table = pd.DataFrame(cat_list, columns = ['Item Name', 'Item Identifier', 'Item ID', 'Item Domain'])
+    pd.set_option('max_columns', 5)
+    pd.set_option('max_colwidth', 200)
+
+    print(table)
+
+get_catalog()
